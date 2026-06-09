ba76dab42f69889c35516662efff36427058a5c6
simgereklam <rollupbannerimalat@gmail.com>
2026-06-09 11:58:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function sendWhatsApp(){
+  let ad=document.getElementById('ad')?.value || "";
+  let tel=document.getElementById('tel')?.value || "";
+  let adres=document.getElementById('adres')?.value || "";
+  let parsel=document.getElementById('parsel')?.value || "";
+  let konu=document.getElementById('konu')?.value || "";
+  let daire=document.getElementById('daire')?.value || "";
+  let mesaj=document.getElementById('mesaj')?.value || "";
+  let text="Merhaba Masfey İnşaat,%0A%0AÖn başvuru yapmak istiyorum.%0A%0AAd Soyad: "+encodeURIComponent(ad)+"%0ATelefon: "+encodeURIComponent(tel)+"%0AAdres: "+encodeURIComponent(adres)+"%0AAda Parsel: "+encodeURIComponent(parsel)+"%0AKonu: "+encodeURIComponent(konu)+"%0ABağımsız Bölüm: "+encodeURIComponent(daire)+"%0AMesaj: "+encodeURIComponent(mesaj);
+  window.open("https://wa.me/905431813485?text="+text,"_blank");
+}
